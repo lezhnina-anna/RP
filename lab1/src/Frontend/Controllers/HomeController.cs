505d6046139660a7a57473cb11d5237c595d795e
lezhnina-anna <Ankemova@gmail.com>
2019-02-12 23:24:23 +0300

    add lab1

---
+++ lab1/src/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Frontend.Models;
+using System.Net.Http;
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(string data)
+        {
+            string id = await GetResponse(data); 
+            Console.WriteLine("id: "+ id);
+            return Ok(id);
+        }
+
+        private async Task<string> GetResponse(string data)
+        {
+            string url = "http://localhost:5000/api/values";
+            HttpContent content = new StringContent(data);
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.PostAsync(url, content);
+            string result = await response.Content.ReadAsStringAsync();
+            return result;
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
